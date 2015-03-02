64a4a812cf107a3b2ee7551de0371939caeec143
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-03 09:38:00 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var Observ = require('observ');
+var ObservStruct = require('observ-struct');
+var URL = require('feature/detect')('URL');
+
+/**
+  # observ-remotemedia
+
+  This is an observable structure that can be used to fetch remote media
+  objects using [`binary-xhr`](https://github.com/maxogden/binary-xhr)
+  and convert them into local object urls for rendering.
+
+  ## Why?
+
+  This module exists as it is makes it possible to write your code in a
+  way that works for both simple webapp that will be rendered in the
+  browser and also
+**/
+module.exports = function(initialVal) {
+  var remote = Observ();
+  var local = Observ();
+
+  function set(val) {
+    if (typeof val == 'string' || (val instanceof String)) {
+      return remote.set(val);
+    }
+
+    _set(val);
+  }
+
+  s = ObservStruct({ remote: remote, local: local });
+  _set = s.set;
+  s.set = set;
+
+  if (initialVal) {
+    set(initialVal);
+  }
+
+  return s;
+};
